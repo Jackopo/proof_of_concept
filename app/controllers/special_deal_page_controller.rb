be1c14829ad5c59e32a2238acb51523cd10bc2e7
Jacopo Ghisolfi <jacopo.ghisolfi@infopark.de>
2017-01-26 13:54:18 +0100

    if valid_from or valid_until is not valid SpecialDealPages render 404

--- app/controllers/special_deal_page_controller.rb
+++ app/controllers/special_deal_page_controller.rb
@@ -1,2 +1,10 @@
 class SpecialDealPageController < CmsController
+
+  before_filter only: :index do
+    if !scrivito_in_editable_view? &&
+      ((@obj.valid_from && @obj.valid_from > Time.zone.now) ||
+      (@obj.valid_until && @obj.valid_until < Time.zone.now))
+      render :expired, status: 404
+    end
+  end
 end
