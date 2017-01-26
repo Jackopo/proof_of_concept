369dab6ed32994bf15f09a8a9a141123fdf9b556
Jacopo Ghisolfi <jacopo.ghisolfi@infopark.de>
2017-01-26 17:27:29 +0100

    Renamed method

--- app/models/special_deal_page.rb
+++ app/models/special_deal_page.rb
@@ -5,4 +5,9 @@ class SpecialDealPage < Obj
   attribute :tags, :stringlist
   attribute :valid_from, :date
   attribute :valid_until, :date
+
+  def valid?
+    return true if self.valid_from.presence > Time.zone.now && self.valid_until.presence > Time.zone.now
+    false
+  end
 end
